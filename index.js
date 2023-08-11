ffd2fac43643d5072227388bd734079b8e857f81
shm_usuf <shmusuf@gmail.com>
2023-08-11 15:04:11 +0600

    change index

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors({ credentials: true }));
 
 /* bypass api */
-app.use("/product", productRoute);
+app.use("api/products", productRoute);
 
 /* connect to database */
 mongoose
